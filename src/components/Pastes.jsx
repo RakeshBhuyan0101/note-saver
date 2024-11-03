95fc0b51096fc55fa72db442d081390ae3ce64bc
web-dev-cohort <rakeshbhuyan0101@gmail.com>
2024-11-03 23:40:58 +0530

    minor bug fixes

--- src/components/Pastes.jsx
+++ src/components/Pastes.jsx
@@ -27,11 +27,11 @@ function Pastes() {
     dispatch(removeFromPaste(pasteId));
   }
   return (
-    <div className="bg-[#1E201E] h-screen">
+    <div className="bg-[#1E201E] md:h-screen">
       <div className="flex items-center justify-center pt-5 ">
         <input
           type="search"
-          className="p-2 rounded-2xl min-w-[500px] bg-[#3C3D37]"
+          className="p-2 rounded-2xl w-screen bg-[#3C3D37]"
           placeholder="Search Paste"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
